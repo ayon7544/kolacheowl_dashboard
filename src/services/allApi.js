f77ebcded902ba5bd0dacc253c0815d95d100153
ayon7544 <ayonghosh1114@gmail.com>
2026-01-16 14:23:49 +0600

    set

--- src/services/allApi.js
+++ src/services/allApi.js
@@ -4,8 +4,7 @@ import { getCookie } from "./cookies";
 const allApi = createApi({
   reducerPath: "allApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://nessa-kolacheowl-backend.vercel.app/api/v1",
-    baseUrl: "http://10.10.20.2:5006/api/v1",
+    baseUrl: "https://nessa-kolacheowl-backend.vercel.app/api/v1",
     prepareHeaders: (headers) => {
       const token = getCookie("NessasBrokenWorldAuthToken");
       if (token) {
